a4c8c6b5678219c1ad03f1e1f680fc57ece630b0
Dorris Zhang <ruixuan.zhang@kyligence.io>
2022-11-28 13:23:01 +0800

    KE-40276 skip shard pruning for in expr

--- src/core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ src/core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -3731,4 +3731,8 @@ public abstract class KylinConfigBase implements Serializable {
     public int getProjectMergeRuleBloatThreshold() {
         return Integer.parseInt(getOptional("kylin.query.project-merge-bloat-threshold", "0"));
     }
+
+    public boolean skipShardPruningForInExpr() {
+        return Boolean.parseBoolean(getOptional("kylin.query.skip-shard-pruning-for-in", FALSE));
+    }
 }
