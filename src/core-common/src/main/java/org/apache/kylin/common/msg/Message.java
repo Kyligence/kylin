4d103f04992a9418e72d830c15b9b144642f50f4
jlf <1251489546@qq.com>
2024-10-15 19:43:27 +0800

    KE-44367 OpenAPI check gluten is enabled (#32140)
    * KE-44367 OpenAPI check gluten is enabled
    
    * KE-44367 rename getGlutenNotEnabled to getGlutenDisabled

--- src/core-common/src/main/java/org/apache/kylin/common/msg/Message.java
+++ src/core-common/src/main/java/org/apache/kylin/common/msg/Message.java
@@ -198,7 +198,7 @@ public class Message {
         return "Invalid internal table parameters.";
     }
 
-    public String getGlutenNotEnabled() {
+    public String getGlutenDisabled() {
         return "Gluten is disabled in query function, please enable it and try again.";
     }
 
@@ -474,6 +474,10 @@ public class Message {
 
     public String getNotInternalTable() {
         return "Table \"%s\" is not an internal table. Please check and try again.";
+    }
+
+    public String getInternalTableDisabled() {
+        return "InternalTable is disabled, please enable it and try again.";
     }
 
     public String getSamplingFailedForIllegalTableName() {
