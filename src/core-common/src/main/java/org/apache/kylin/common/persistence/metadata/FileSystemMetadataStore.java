c4d504c03c49062c856a730dce16a4b53c7a17f0
夏旭晨 <37445028+xiaxuchen@users.noreply.github.com>
2024-11-19 19:09:23 +0800

    KE-44496 fix parallel build/refresh job (#32274)
    * KE-44496 fix parallel build/refresh job
    
    * KE-44496 fix bug for discard job
    
    KE-44496 fix parallel build/refresh job (#32261)
    
    KE-44423 fix ut using transaction (#32251)
    
    Co-authored-by: xuchen.xia <xuchen.xia@kyligence.io>

--- src/core-common/src/main/java/org/apache/kylin/common/persistence/metadata/FileSystemMetadataStore.java
+++ src/core-common/src/main/java/org/apache/kylin/common/persistence/metadata/FileSystemMetadataStore.java
@@ -228,6 +228,13 @@ public class FileSystemMetadataStore extends MetadataStore {
         List<T> resList;
         val context = convertConditionsToFilter(filter, type);
         Class<T> resourceClass = (Class<T>) type.getResourceClass();
+        if (needLock) {
+            if (context.isWholePath()) {
+                lockResource(context.getResPath());
+            } else {
+                lockResource(type.name());
+            }
+        }
         try {
             if (this.type == Type.DIR) {
                 resList = getFromDir(context, needContent, resourceClass);
@@ -236,13 +243,6 @@ public class FileSystemMetadataStore extends MetadataStore {
             }
         } catch (IOException e) {
             throw new KylinRuntimeException("get resource fail", e);
-        }
-        if (needLock) {
-            if (context.isWholePath()) {
-                lockResource(context.getResPath());
-            } else {
-                lockResource(type.name());
-            }
         }
         return resList;
     }
